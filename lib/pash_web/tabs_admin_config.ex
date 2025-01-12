7c0f604882f245b9e49402c7884126232cf6ff4f
tm <tm.oxenfree@gmail.com>
2025-01-12 19:09:02 +0700

    merge auth

---
+++ lib/pash_web/tabs_admin_config.ex
@@ -0,0 +1,32 @@
+defmodule PashWeb.TabsAdminConfig do
+  @moduledoc false
+
+  use PashWeb, :verified_routes
+  use Gettext, backend: PashWeb.Gettext
+
+  def view_admin_groups do
+    %{
+      home: [PashWeb.HomeLive],
+      blog: [PashWeb.PostLive],
+      admin: [PashWeb.AdminLive]
+      # products: [PashWeb.ProductLive.Index, PashWeb.ProductLive.Show],
+      # stock: [PashWeb.StockLive],
+      # clients: [PashWeb.ClientLive],
+      # account: [PashWeb.AccountLive],
+      # notifications: [PashWeb.NotificationLive]
+    }
+  end
+
+  def tabs_admin_config do
+    [
+      %{
+        label: gettext("Admin"),
+        icon: "hero-user-solid",
+        tab: :admin,
+        options: [navigate: ~p"/admin"],
+        separator: true
+        # badge: 1
+      }
+    ]
+  end
+end
